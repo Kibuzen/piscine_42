2fa6663bd4abf1f9568f439fef9b99f8a07a782f
Quentin JACOB <qujacob@e3r5p20.42.fr>
2021-02-17 08:51:52 +0100

    c03 100%

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qujacob <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/07 13:16:30 by qujacob           #+#    #+#             */
+/*   Updated: 2021/02/07 14:25:59 by qujacob          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int				ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	j = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (dest[i] != '\0' && i < size)
+		i++;
+	if (i < size)
+	{
+		while (j + i < size - 1 && src[j] != '\0')
+		{
+			dest[i + j] = src[j];
+			j++;
+		}
+		dest[i + j] = '\0';
+	}
+	return (ft_strlen(src) + i);
+}
